8f496ac482f7c35c570af528c8fa65b5f8608d9a
musaubrian <musaubrian15@gmail.com>
2022-08-26 23:13:40 +0300

    create binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -2,7 +2,7 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
-
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -24,4 +24,5 @@ typedef struct binary_tree_s heap_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s bst_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif /* _BINARY_TREES_H_ */
